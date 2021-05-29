62ac474417eafe2fa72c3d70e1ba47319949c86d
Alexander Simko <alexander.ch.simko@gmail.com>
2021-05-29 12:35:40 +0100

    Configured npm and webpacker

---
+++ static/webpack.config.js
@@ -0,0 +1,22 @@
+const webpack = require('webpack');
+const resolve = require('path').resolve;
+
+const config = {
+    devtool: 'eval-source-map',
+    entry: __dirname + '/js/script.js',
+    output:{
+        path: __dirname + '/dist',
+        filename: 'bundle.js',
+    },
+        resolve: {
+        extensions: ['.js','.jsx','.css']
+    },
+    module: {
+        rules: [{
+            test: /\.jsx?/,
+            loader: 'babel-loader',
+            exclude: /node_modules/,
+        }]
+    }
+};
+module.exports = config;
